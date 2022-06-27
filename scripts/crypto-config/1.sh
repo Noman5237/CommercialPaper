99404a3107a7714ebcc762032615d6d1c7329a18
Noman5237 <anonyman637@gmail.com>
2022-06-27 15:33:34 +0600

    generating certficates for orderers and peer orgs

---
+++ scripts/crypto-config/1.sh
@@ -0,0 +1,2 @@
+# Generate networks first orderer crypto
+cryptogen generate --output=$PWD/artifacts/crypto-config --config=$PWD/configs/crypto-config/orderer.yaml
---
+++ scripts/crypto-config/2.sh
@@ -0,0 +1,2 @@
+# Generate networks peer organizations
+cryptogen extend --input=$PWD/artifacts/crypto-config --config=$PWD/configs/crypto-config/peers.yaml
